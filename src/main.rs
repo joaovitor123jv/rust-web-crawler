49de55269883109c37830e2b3dfd8d7eca08e5c2
Joao Vitor Antoniassi Segantin <segantin.jv@gmail.com>
2020-07-27 22:35:47 -0300

    Validação inicial de email com automato

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,46 @@ fn test_chain(chain: &str, dfa: &lib::DFA) {
     println!("O resultado do teste da cadeia {} foi {}", chain, dfa.test(chain).unwrap());
 }
 
+fn add_generic_transition(origin_state: &str, destiny_state: &str, dfa: &mut lib::DFA) {
+    dfa.add_transition(origin_state, destiny_state, '0').unwrap();
+    dfa.add_transition(origin_state, destiny_state, '1').unwrap();
+    dfa.add_transition(origin_state, destiny_state, '2').unwrap();
+    dfa.add_transition(origin_state, destiny_state, '3').unwrap();
+    dfa.add_transition(origin_state, destiny_state, '4').unwrap();
+    dfa.add_transition(origin_state, destiny_state, '5').unwrap();
+    dfa.add_transition(origin_state, destiny_state, '6').unwrap();
+    dfa.add_transition(origin_state, destiny_state, '7').unwrap();
+    dfa.add_transition(origin_state, destiny_state, '8').unwrap();
+    dfa.add_transition(origin_state, destiny_state, '9').unwrap();
+
+
+    dfa.add_transition(origin_state, destiny_state, 'z').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'y').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'x').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'w').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'v').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'u').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 't').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 's').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'r').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'q').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'p').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'o').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'n').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'm').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'l').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'k').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'j').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'i').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'h').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'g').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'f').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'e').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'd').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'c').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'b').unwrap();
+    dfa.add_transition(origin_state, destiny_state, 'a').unwrap();
+}
 
 fn main() {
     println!("Testando importação de funcoes");
@@ -15,66 +55,38 @@ fn main() {
         &["user", "at", "provider", "user_type", "trash"]
     ).unwrap();
 
-    dfa.add_transition("user", "user", '0').unwrap();
-    dfa.add_transition("user", "user", '1').unwrap();
-    dfa.add_transition("user", "user", '2').unwrap();
-    dfa.add_transition("user", "user", '3').unwrap();
-    dfa.add_transition("user", "user", '4').unwrap();
-    dfa.add_transition("user", "user", '5').unwrap();
-    dfa.add_transition("user", "user", '6').unwrap();
-    dfa.add_transition("user", "user", '7').unwrap();
-    dfa.add_transition("user", "user", '8').unwrap();
-    dfa.add_transition("user", "user", '9').unwrap();
+    add_generic_transition("trash", "trash", &mut dfa);
+    dfa.add_transition("trash", "trash", '@').unwrap();
+    dfa.add_transition("trash", "trash", '.').unwrap();
 
 
-    dfa.add_transition("user", "user", 'z').unwrap();
-    dfa.add_transition("user", "user", 'y').unwrap();
-    dfa.add_transition("user", "user", 'x').unwrap();
-    dfa.add_transition("user", "user", 'w').unwrap();
-    dfa.add_transition("user", "user", 'v').unwrap();
-    dfa.add_transition("user", "user", 'u').unwrap();
-    dfa.add_transition("user", "user", 't').unwrap();
-    dfa.add_transition("user", "user", 's').unwrap();
-    dfa.add_transition("user", "user", 'r').unwrap();
-    dfa.add_transition("user", "user", 'q').unwrap();
-    dfa.add_transition("user", "user", 'p').unwrap();
-    dfa.add_transition("user", "user", 'o').unwrap();
-    dfa.add_transition("user", "user", 'n').unwrap();
-    dfa.add_transition("user", "user", 'm').unwrap();
-    dfa.add_transition("user", "user", 'l').unwrap();
-    dfa.add_transition("user", "user", 'k').unwrap();
-    dfa.add_transition("user", "user", 'j').unwrap();
-    dfa.add_transition("user", "user", 'i').unwrap();
-    dfa.add_transition("user", "user", 'h').unwrap();
-    dfa.add_transition("user", "user", 'g').unwrap();
-    dfa.add_transition("user", "user", 'f').unwrap();
-    dfa.add_transition("user", "user", 'e').unwrap();
-    dfa.add_transition("user", "user", 'd').unwrap();
-    dfa.add_transition("user", "user", 'c').unwrap();
-    dfa.add_transition("user", "user", 'b').unwrap();
-    dfa.add_transition("user", "user", 'a').unwrap();
+    add_generic_transition("at", "provider", &mut dfa);
+    dfa.add_transition("at", "trash", '@').unwrap();
+    dfa.add_transition("at", "trash", '.').unwrap();
 
+    add_generic_transition("provider", "provider", &mut dfa);
+    dfa.add_transition("provider", "user_type", '.').unwrap();
+    dfa.add_transition("provider", "trash", '@').unwrap();
 
+    add_generic_transition("user_type", "user_type", &mut dfa);
+    dfa.add_transition("user_type", "trash", '@').unwrap();
+    dfa.add_transition("user_type", "trash", '.').unwrap();
 
-    dfa.add_transition("Q2", "Q4", '1').unwrap();
-    dfa.add_transition("Q2", "Q1", '0').unwrap();
+    add_generic_transition("user", "user", &mut dfa);
+    dfa.add_transition("user", "at", '@').unwrap();
 
-    dfa.add_transition("Q3", "Q4", '1').unwrap();
-    dfa.add_transition("Q3", "Q1", '0').unwrap();
-
-    dfa.add_transition("Q4", "Q1", '1').unwrap();
-    dfa.add_transition("Q4", "Q3", '0').unwrap();
 
     dfa.show();
 
     // Aceita
     // Rejeita
     
-    test_chain("01", &dfa);
-    test_chain("1001", &dfa);
-    test_chain("10011", &dfa);
-    test_chain("", &dfa);
-    test_chain("0", &dfa);
-    test_chain("101010100101010010101001", &dfa);
+    test_chain("akjdfhiowb", &dfa);
+    test_chain("akjdfhiowb@asdlk.com", &dfa);
+    test_chain("akjdfhiowb@asdlkcom", &dfa);
+    test_chain("@asdlkcom.asd", &dfa);
+
+    test_chain("sgreyluy@twitch.com", &dfa);
+    
 }
 
