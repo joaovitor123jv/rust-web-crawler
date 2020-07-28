b753e6197b0ec942816dd3ff1eca9e217d24686c
Joao Vitor Antoniassi Segantin <segantin.jv@gmail.com>
2020-07-27 21:09:02 -0300

    Adds test function to dfa

--- src/lib/dfa.rs
+++ src/lib/dfa.rs
@@ -1,12 +1,19 @@
 use std::collections::HashSet;
 
+#[derive(Hash, Eq, PartialEq, Debug)]
+pub struct Transition {
+    from: String,
+    to: String,
+    when: char
+}
+
 // Deterministic Finite Automaton
 pub struct DFA {
     initial_state: String,
     final_state: String,
     alphabet: HashSet<char>,
-    states: HashSet<String> // Conjunto de Estados
-    // transitions: ??? // Função de transição
+    states: HashSet<String>, // Conjunto de Estados
+    transitions: HashSet<Transition> // Função de transição
 }
 
 impl DFA {
@@ -36,11 +43,59 @@ impl DFA {
             initial_state: initial_state.to_string(),
             final_state: final_state.to_string(),
             alphabet: obtained_alphabet,
-            states: obtained_states
+            states: obtained_states,
+            transitions: HashSet::new()
         })
     }
 
+    pub fn add_transition(&mut self, from_state: &str, to_state: &str, symbol: char) -> Result<bool, String> {
+        if !self.states.contains(from_state) { 
+            return Err("O estado de origem precisa pertencer ao conjunto de estados do automato".to_string());
+        }
+
+        if !self.states.contains(to_state) { 
+            return Err("O estado de destino precisa pertencer ao conjunto de estados do automato".to_string());
+        }
+
+        if !self.alphabet.contains(&symbol) { 
+            return Err("O simbolo recebido precisa pertencer ao alfabeto do automato".to_string());
+        }
+
+        self.transitions.insert(Transition {
+            from: from_state.to_string(),
+            to: to_state.to_string(),
+            when: symbol
+        });
+
+        Ok(true)
+    }
+
+    pub fn test(&self, input_chain: &str) -> bool {
+        let mut actual_state: String = self.initial_state.clone();
+
+        for input in input_chain.chars() {
+            if !self.alphabet.contains(&input) {
+                return false;
+            }
+
+            let next_transition: Vec<&Transition> = self.transitions.iter().filter(|&t| (t.when == input) && (t.from == actual_state)).collect();
+
+            if next_transition.first() == None {
+                return false;
+            }
+
+            actual_state = next_transition.first().unwrap().to.clone();
+        }
+
+        if actual_state == self.final_state {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     pub fn show(&self) {
+        println!("-----------------");
         println!("O estado inicial é: {}, O estado final é: {}", self.initial_state, self.final_state);
 
         println!("\nOs estados do DFA são:");
@@ -50,12 +105,18 @@ impl DFA {
         }
 
 
-        println!("O alfabeto é composto pelos símbolos:");
+        println!("\nO alfabeto é composto pelos símbolos:");
         for symbol in &self.alphabet {
             print!(" {} ", symbol);
         }
 
-        println!("");
+        println!("\nAs transições disponíveis são:");
+
+        for transition in &self.transitions {
+            println!(" {}({}): {} ", transition.from, transition.when, transition.to);
+        }
+
+        println!("-----------------");
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
 mod lib;
 
+fn test_chain(chain: &str, dfa: &lib::DFA) {
+    println!("O resultado do teste da cadeia {} foi {}", chain, dfa.test(chain));
+}
+
 fn main() {
     println!("Testando importação de funcoes");
 
-    let dfa = lib::DFA::new("Q1", "Q2", "aaaaaaabcdef", &["Q1", "Q2", "Q3"]).unwrap();
+    let mut dfa = lib::DFA::new("Q1", "Q4", "01", &["Q1", "Q2", "Q3", "Q4"]).unwrap();
+
+    dfa.add_transition("Q1", "Q2", '1').unwrap();
+    dfa.add_transition("Q1", "Q3", '0').unwrap();
+
+    dfa.add_transition("Q2", "Q4", '1').unwrap();
+    dfa.add_transition("Q2", "Q1", '0').unwrap();
+
+    dfa.add_transition("Q3", "Q4", '1').unwrap();
+    dfa.add_transition("Q3", "Q1", '0').unwrap();
+
+    dfa.add_transition("Q4", "Q1", '1').unwrap();
+    dfa.add_transition("Q4", "Q3", '0').unwrap();
 
     dfa.show();
+
+    // Aceita
+    // Rejeita
+    
+    test_chain("01", &dfa);
+    test_chain("1001", &dfa);
+    test_chain("10011", &dfa);
+    test_chain("", &dfa);
+    test_chain("0", &dfa);
+    test_chain("1010100101010101", &dfa);
 }
-
-
 
